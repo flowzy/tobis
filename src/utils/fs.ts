49122b92224e94e525e7530ace38c8ac98d8c0eb
flowzy <flowzyd@gmail.com>
2023-09-20 23:20:55 +0300

    fix(utils): glob path in readDir function

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -4,5 +4,5 @@ import { glob } from 'glob';
 const ROOT_DIR = path.dirname(Bun.main);
 
 export async function readDir(dir: string) {
-	return glob(path.join(ROOT_DIR, dir));
+	return glob(path.join(ROOT_DIR, 'src', dir));
 }
